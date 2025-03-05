13def12813d6ccb0d80cd7f3ad4c7ed61973da83
toothstickys <81208571+toothstickys@users.noreply.github.com>
2025-03-05 14:47:20 +0800

    Create minimax.py

---
+++ models/minimax.py
@@ -0,0 +1,69 @@
+import json
+import requests
+import time
+
+def generate_MinMax(argparsers, text, gender):
+    if gender == 'female':
+        voice = argparsers.female_voice
+        pitch = argparsers.female_tune
+        speed = argparsers.female_speed
+        volume = argparsers.female_volume
+    else:
+        voice = argparsers.male_voice
+        pitch = argparsers.male_tune
+        speed = argparsers.male_speed
+        volume = argparsers.male_volume
+
+    #调用minmax接口
+    group_id = '1862062097068724524'  # 请输入您的group_id
+    api_key = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJHcm91cE5hbWUiOiIxMTEiLCJVc2VyTmFtZSI6IjExMSIsIkFjY291bnQiOiIiLCJTdWJqZWN0SUQiOiIxODYyMDYyMDk3MDc3MTEzMTMyIiwiUGhvbmUiOiIxMzkxNDAwNjk3NCIsIkdyb3VwSUQiOiIxODYyMDYyMDk3MDY4NzI0NTI0IiwiUGFnZU5hbWUiOiIiLCJNYWlsIjoiIiwiQ3JlYXRlVGltZSI6IjIwMjQtMTItMTAgMTc6MzM6MDAiLCJUb2tlblR5cGUiOjEsImlzcyI6Im1pbmltYXgifQ.GTY7YaODs0a9JwYWZz3TSGtBLvJcgfK-IWzgCe_RoFFqOcd2NUXQjm93Sy7IJIhpYD_kgBrXFrmE6yzOYJvexI6KbURk_KtKhWiaGL_8kDju0s5W7UuNCMOSj_mEChENeprHpHM6nMD8iZLhxM1vazQ2Utmwv899K6heAoCoIrWy1C0CSx4eoVi_tOIatcCQ1E550f-vzyWRnKkT8tbOreBebgIiaWxTSbGRCn3WsPqCykAUrLsmNj0B6dBTG5NjoZlYoj4aCfIBRr17S2MuEhx4qk25m1tp7iIm-OGp9gsstFQeHECxTIkRoRE2rlKt3YTsDnBauoxuuJMFPKYQzQ'
+    url = f"https://api.minimax.chat/v1/t2a_v2?GroupId={group_id}"
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json",
+    }
+    data = json.dumps({
+        # 如同时传入voice_id和timber_weights时，则会自动忽略voice_id，以timber_weights传递的参数为准
+        "text": text,
+        "model": argparsers.MM_model,
+        "stream": False,
+
+        "timber_weights": [
+            {
+                "voice_id": voice,
+                "weight": 80
+            },
+            # {
+            #     "voice_id": argparsers.Voice2,
+            #     "weight": argparsers.Weight2
+            # }
+        ],        "voice_setting": {
+            "voice_id": '',
+            "speed": speed,
+            "vol": volume,
+            "pitch": pitch,
+            "emotion": 'neutral'
+        },
+        "audio_setting": {
+            "sample_rate": 32000,
+            "bitrate": 128000,
+            "format": "wav"
+        }
+    })
+    while True:
+        #有时会限制访问，需要进行循环操作来处理
+        response = requests.request("POST", url, stream=True, headers=headers, data=data)
+        resp_json = json.loads(response.text)
+        time.sleep(0.3)
+        print(resp_json['base_resp']['status_msg'])
+        print(text)
+        if resp_json['base_resp']['status_msg'] == "invalid params, empty field":
+            print(text)
+            #判断如果不再输入文本则直接退出
+            return "0"
+        try:
+            audio_value = bytes.fromhex(resp_json['data']['audio'])
+            break
+        except KeyError:
+            continue
+    return audio_value
